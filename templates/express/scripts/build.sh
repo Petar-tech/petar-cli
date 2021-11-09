4967b8855601a07c8d0baf609cda5ac9a3b71038
Petar Paunovic <58889347+Petar-tech@users.noreply.github.com>
2021-11-09 03:13:21 +0100

    updated build script

--- templates/express/scripts/build.sh
+++ templates/express/scripts/build.sh
@@ -1,7 +1,53 @@
-SERVICE_VERSION="$(cat .env | grep SERVICE_VERSION)"
-serviceVersionArr=(${SERVICE_VERSION//=/ })
+#!/bin/bash
+
+IFS='='
 
 SERVICE_NAME="$(cat .env | grep SERVICE_NAME)"
-serviceNameArr=(${SERVICE_NAME//=/ })
+CURRENT_NAME=""
+for x in $SERVICE_NAME
+do
+	if [ "${x}" != "SERVICE_NAME" ]
+	then
+		CURRENT_NAME=$x
+		break
+	fi
+done
 
-eval "docker build . -t ${serviceNameArr[1]}:${serviceVersionArr[1]}"
+echo "Service name: $CURRENT_NAME"
+
+SERVICE_VERSION="$(cat .env | grep SERVICE_VERSION)"
+CURRENT_VERSION=""
+for x in $SERVICE_VERSION
+do
+	if [ "${x}" != "SERVICE_VERSION" ]
+	then
+		CURRENT_VERSION=$x
+		break
+	fi
+done
+
+DOCKER_HUB_ENV_USERNAME="$(cat .env | grep DOCKER_HUB_USERNAME)"
+DOCKER_HUB_USERNAME=""
+for x in $DOCKER_HUB_ENV_USERNAME
+do
+	if [ "${x}" != "DOCKER_HUB_USERNAME" ]
+	then
+		DOCKER_HUB_USERNAME=$x
+		break
+	fi
+done
+
+DOCKER_HUB_ENV_PASSWORD="$(cat .env | grep DOCKER_HUB_PASSWORD)"
+DOCKER_HUB_PASSWORD=""
+for x in $DOCKER_HUB_ENV_PASSWORD
+do
+	if [ "${x}" != "DOCKER_HUB_PASSWORD" ]
+	then
+		DOCKER_HUB_PASSWORD=$x
+		break
+	fi
+done
+
+eval "docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD"
+eval "docker build . -t $DOCKER_HUB_USERNAME/$CURRENT_NAME:$CURRENT_VERSION"
+eval "docker push $DOCKER_HUB_USERNAME/$CURRENT_NAME:$CURRENT_VERSION"
